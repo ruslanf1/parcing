6ecda7f7161fba83f08b56a930e34020804e27ce
ruslanf1 <62360818+ruslanf1@users.noreply.github.com>
2020-03-19 02:00:59 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+require_once('include/include.php');
+
+$db = db_connect();
+$sql = "SELECT * FROM ruslan";
+$result = $db->query($sql);
+$db->close();
+
+$element = array();
+for ($i = 0; $i < $result->num_rows; $i++) {
+    $element[] = $result->fetch_assoc();
+}
+
+$arr = ['news' => $element];
+
+echo $twig->render('index.html', $arr);
+
